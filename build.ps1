aa628a1f51dab48209d3679866b4f70ceba1d471
Denis Sidenko <denis.sidenko@pm.bet>
2019-09-22 16:47:47 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -14,34 +14,26 @@ $root = (Get-Item -Path ".\").FullName
 $artifactsPath = $root + "\artifacts"
 if(Test-Path $artifactsPath) { Remove-Item $artifactsPath -Force -Recurse }
 
-$branch = @{ $true = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH; $false = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL] }[$env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-$commitHash = $(git rev-parse --short HEAD)
-$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
-$versionSuffix = @{ $true = "--version-suffix=$($suffix)"; $false = ""}[$suffix -ne ""]
+$output = gitversion /nofetch | Out-String
+$versions = $output | ConvertFrom-Json
+$packageVer = $versions.NuGetVersion
+$buildVer = $versions.SemVer
 
-gitversion /l console /output buildserver /updateAssemblyInfo
+echo "Build: Package version $packageVer"
+echo "Build: Build version $buildVer"
 
-echo "Build: Package version suffix is $suffix"
-echo "Build: Build version suffix is $buildSuffix"
+exec { & gitversion /output buildserver /updateAssemblyInfo}
 
 # Update Appveyor version
-if (Test-Path env:APPVEYOR) {
-    $props = [xml](Get-Content "src\Directory.Build.props")
-    $prefix = $props.Project.PropertyGroup.VersionPrefix
-    
-    $avSuffix = @{ $true = $($suffix); $false = $props.Project.PropertyGroup.VersionSuffix }[$suffix -ne ""]
-    $full = @{ $true = "$($prefix)-$($avSuffix)"; $false = $($prefix) }[-not ([string]::IsNullOrEmpty($avSuffix))]
-    
-    echo "Build: Full version is $full"
-    Update-AppveyorBuild -Version $full
+if (Test-Path env:APPVEYOR) {      
+#	echo "Build: Full version is $full"
+    Update-AppveyorBuild -Version $buildVer
 }
 
 # Build
 echo "`n`n----- BUILD -----`n"
 
-exec { & dotnet build DSFrameworkCore.sln -c Release --version-suffix=$buildSuffix }
+exec { & dotnet build DSFrameworkCore.sln -c Release -p:Version=$buildVer }
 
 # Test
 #echo "`n`n----- TEST -----`n"
@@ -68,4 +60,4 @@ exec { & dotnet build DSFrameworkCore.sln -c Release --version-suffix=$buildSuff
 # Pack
 echo "`n`n----- PACK -----`n"
 
-exec { & dotnet pack -c Release -o $artifactsPath --include-symbols --no-build $versionSuffix }
+exec { & dotnet pack -c Release -o $artifactsPath --include-symbols --no-build $packageVer }
