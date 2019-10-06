3d83788cee5cdaebd5fac91defba5b6504060726
Denis Sidenko <denis.sidenko@pm.bet>
2019-10-06 08:42:46 +0300

    Add DeleteCommand

---
+++ src/DSFramework.Contracts.Common/DeleteCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace DSFramework.Contracts.Common
+{
+    public class DeleteCommand<TIdentity>
+    {
+        public TIdentity EntityId { get; set; }
+
+        public string Application { get; set; }
+
+        public string User { get; set; }
+
+        public string OperationId { get; set; }
+    }
+}
