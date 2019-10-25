48ec5cc81fcf8a4cb693bb3c9662425e03de12d7
Denis Sidenko <denis.sidenko@pm.bet>
2019-10-25 15:15:51 +0300

    Add GenericExecutor

---
+++ src/DSFramework.Threading/DataFlow/SafeActionBlock.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.Extensions.Logging;
+
+namespace DSFramework.Threading.DataFlow
+{
+    public class SafeActionBlock<TInput> : ITargetBlock<TInput>
+    {
+        private readonly Func<TInput, Task> _action;
+        private readonly ActionBlock<TInput> _actionBlock;
+        private readonly ILogger _logger;
+
+        public Task Completion => _actionBlock.Completion;
+
+        public int InputCount => _actionBlock.InputCount;
+
+        public SafeActionBlock(ILogger logger, Action<TInput> action, ExecutionDataflowBlockOptions dataFlowBlockOptions)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            _logger = logger;
+            _action = input =>
+            {
+                action(input);
+                return Task.CompletedTask;
+            };
+            _actionBlock = new ActionBlock<TInput>(ActionAsync, dataFlowBlockOptions);
+        }
+
+        public SafeActionBlock(ILogger logger, Func<TInput, Task> action, ExecutionDataflowBlockOptions dataFlowBlockOptions)
+        {
+            _logger = logger;
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+            _actionBlock = new ActionBlock<TInput>(ActionAsync, dataFlowBlockOptions);
+        }
+
+        public DataflowMessageStatus OfferMessage(
+            DataflowMessageHeader messageHeader,
+            TInput messageValue,
+            ISourceBlock<TInput> source,
+            bool consumeToAccept)
+        {
+            return ((ITargetBlock<TInput>)_actionBlock).OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+
+        public void Complete()
+        {
+            _actionBlock.Complete();
+        }
+
+        public void Fault(Exception exception)
+        {
+            ((IDataflowBlock)_actionBlock).Fault(exception);
+        }
+
+        public override string ToString()
+        {
+            return _actionBlock.ToString();
+        }
+
+        private async Task ActionAsync(TInput input)
+        {
+            try
+            {
+                await _action(input);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(new EventId(-1), e, e.Message);
+            }
+        }
+    }
+}
---
+++ src/DSFramework.Threading/GenericExecutor.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DSFramework.Threading
+{
+    public class GenericExecutor<TKey>
+    {
+        private readonly Dictionary<TKey, ActionCachedAsyncExecutor> _executors = new Dictionary<TKey, ActionCachedAsyncExecutor>();
+        private readonly ILogger _logger;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly TaskScheduler _taskScheduler;
+
+        public GenericExecutor(int maxSimultaneousExecutions = 100)
+            : this(NullLogger.Instance, TaskScheduler.Default, maxSimultaneousExecutions)
+        {
+        }
+
+        public GenericExecutor(ILogger logger, int maxSimultaneousExecutions = 100)
+            : this(logger, TaskScheduler.Default, maxSimultaneousExecutions)
+        {
+        }
+
+        public GenericExecutor(ILogger logger, SemaphoreSlim semaphore)
+            : this(logger, TaskScheduler.Default, semaphore)
+        {
+        }
+
+        public GenericExecutor(TaskScheduler taskScheduler, int maxSimultaneousExecutions = 100)
+            : this(NullLogger.Instance, taskScheduler, new SemaphoreSlim(maxSimultaneousExecutions, maxSimultaneousExecutions))
+        {
+        }
+
+        public GenericExecutor(ILogger logger, TaskScheduler taskScheduler, int maxSimultaneousExecutions = 100)
+            : this(logger, taskScheduler, new SemaphoreSlim(maxSimultaneousExecutions, maxSimultaneousExecutions))
+        {
+        }
+
+        public GenericExecutor(ILogger logger, TaskScheduler taskScheduler, SemaphoreSlim semaphore)
+        {
+            _logger = logger;
+            _taskScheduler = taskScheduler;
+            _semaphore = semaphore;
+        }
+
+        public void TryExecute(TKey key, Action action)
+        {
+            var executor = GetExecutor(key);
+
+            executor.TryExecute(action);
+        }
+
+        public void TryExecute(TKey key, Func<Task> action)
+        {
+            var executor = GetExecutor(key);
+
+            executor.TryExecute(action);
+        }
+
+        private ActionCachedAsyncExecutor GetExecutor(TKey key)
+        {
+            lock (_executors)
+            {
+                if (!_executors.TryGetValue(key, out var executor))
+                {
+                    executor = new ActionCachedAsyncExecutor(_taskScheduler, _logger, _semaphore);
+                    _executors[key] = executor;
+                }
+
+                return executor;
+            }
+        }
+    }
+}
