3dcffd4f8ffebdd19037bd485b0a4066d132579e
Denis Sidenko <denis.sidenko@pm.bet>
2019-11-19 18:53:15 +0200

    Remove GenericManager and rename namespaces

--- src/DSFramework.DataProtection/CryptoUtil/Encryption.cs
+++ src/DSFramework.DataProtection/CryptoUtil/Encryption.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace DSFramework.Security.Cryptography
+namespace DSFramework.DataProtection.CryptoUtil
 {
     public static class Encryption
     {
